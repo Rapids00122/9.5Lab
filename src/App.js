9edf7b70466db9a5b418cee2411157dd560f7ebf
Richard Ford <72162439+Rapids00122@users.noreply.github.com>
2024-03-18 20:55:00 -0400

    Added Richard Ford Name Placement under react logo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Richard Ford Lab 9.5
         </a>
       </header>
     </div>
